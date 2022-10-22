3738e8a3cdd6fc25d5caecc475cd8f6f16fe9a98
shreyarao24 <63833616+shreyarao24@users.noreply.github.com>
2022-10-22 12:10:03 +1100

    updated code

--- DataExtraction/data_extraction.py
+++ DataExtraction/data_extraction.py
@@ -1,4 +1,3 @@
-
 # This file contains the data extraction code for 3 different PDF formats.
 
 # Code 1
@@ -48,7 +47,7 @@ final.to_csv('C:/Users/Shreya/Desktop/ctd files/file-name.csv')
 
 # Code 2
 
-f = pandas.read_csv('C:/Users/Shreya/Desktop/file-name.csv', header=None)
+f = pandas.read_csv('C:/Users/Shreya/Downloads/file-name.csv', header=None)
 
 # dropping duplicate rows - since headers were repeated on every new page of the pdf
 f.drop_duplicates(keep='first', inplace=True)
@@ -62,25 +61,27 @@ for row in f.itertuples():
     while r.count('') > 0:
         r.remove('')
 
-    if r[0].isnumeric() and len(r) > 1:
-        sr_no = r[0]
+    if r[0].isnumeric() and len(r) >= 3:
+        if 'General' not in r and 'OBC(Category' not in r and 'ST' not in r:
+            sr_no = r[0]
 
-        for i in range(0, len(r)):
-            if len(r[i]) == 3 and r[i].isnumeric() and len(r[i + 1]) == 3 and r[i + 1].isnumeric():
-                sid = '*' + r[i] + r[i + 1] + r[i + 2]
-                break
+            for i in range(0, len(r)):
+                if len(r[i]) == 3 and r[i].isnumeric() and len(r[i + 1]) == 3 and r[i + 1].isnumeric():
+                    sid = '*' + r[i] + r[i + 1] + r[i + 2]
+                    break
 
-        name = r[1]
-        if r[2].isalpha():
-            name = name + ' ' + r[2]
+            if r[2].isalpha():
+                name = r[1] + ' ' + r[2]
+            else:
+                name = r[1]
 
-        if 'BOY' in r:
-            gender = 'BOY'
-        else:
-            gender = 'GIRL'
+            if 'BOY' in r:
+                gender = 'BOY'
+            else:
+                gender = 'GIRL'
 
-        l = [sr_no, name, sid, gender]
-        final.loc[len(final)] = l
+            l = [sr_no, name, sid, gender]
+            final.loc[len(final)] = l
 
 final.to_csv('C:/Users/Shreya/Desktop/files/file-name.csv')
 
--- Squid Game simulation/squid_game.py
+++ Squid Game simulation/squid_game.py
@@ -1,8 +1,69 @@
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.style.use('ggplot')
+plt.style.use('seaborn')
 
-full_runs = {}
+col_names = ['Player 1', 'Player 2', 'Player 3', 'Player 4', 'Player 5', 'Player 6', 'Player 7', 'Player 8', 'Player 9', 'Player 10', 'Player 11', 'Player 12', 'Player 13', 'Player 14', 'Player 15', 'Player 16']
 
-for p in range(0, 15):
+full_runs = pd.DataFrame(columns = col_names)
+
+for r in range(0, 10):
+
+    all_steps = []
+    completed = []
+
+    for p in range(0, 16):
+
+        if p == 0:
+            steps = []
+            for s in range(0, 18):
+                step = random.randint(0, 1)
+
+                if step == 1:
+                    steps.append(step)
+                else:
+                    all_steps.append(steps)
+                    completed.append(len(steps))
+                    break
+
+        else:
+            steps = all_steps[p - 1] + [1]
+
+            for s in range(len(steps) , 18):
+                step = random.randint(0, 1)
+
+                if step == 1:
+                    steps.append(step)
+                else:
+                    all_steps.append(steps)
+                    completed.append(len(steps))
+                    break
+
+        if len(steps) == 18:
+            completed.append(len(steps))
+            all_steps.append(steps)
+            break
+
+    last_completed = completed.index(completed[-1])
+
+    if len(completed) < 16:
+        for i in range(last_completed + 1, 16):
+            completed.append(18)
+
+    full_runs.loc[len(full_runs.index)] = completed
+
+nums_successful = {}
+
+for p in col_names:
+    nums_successful[p] = full_runs[p].tolist().count(18)
+
+nums_df = pd.DataFrame.from_dict(nums_successful, orient = 'index')
+nums_df.rename(columns = {0:'frequency'}, inplace=True)
+nums_df['probability'] = nums_df['frequency']/10
+nums_df['players'] = col_names
+
+plt.bar(nums_df['players'], nums_df['probability'])
+plt.xticks(rotation = 90)
+plt.ylabel('Frequency of success')
+plt.tick_params(axis='x', which='major', labelsize=7)
+plt.show()
